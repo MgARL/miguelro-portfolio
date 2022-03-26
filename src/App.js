7658a05efc2d8bf7ed651c507d747a42f3d4b404
Miguel(MgARL) <mgarlcr@gmail.com>
2022-03-26 15:26:57 -0400

    Removed duplicate display key

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box'
 
 function App() {
   return (
-    <Box component="span" sx={{ display: 'block', display: 'flex', flexDirection: 'column', alignItems:'center' }} className="App">
+    <Box component="span" sx={{display: 'flex', flexDirection: 'column', alignItems:'center' }} className="App">
       <MyInfo />
       <Info />
     </Box>
